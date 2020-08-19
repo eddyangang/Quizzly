387cba04b5bb7d7e716cf0fe52416ebd53a86a5a
eddyangang <eddyyang@berkeley.edu>
2020-08-18 19:03:28 -0700

    refactored code so that it would better pass down data to components

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,59 +1,16 @@
-import React, { useState, useEffect } from "react";
-import queryString from 'query-string';
-import io from "socket.io-client";
-
+import React, { useContext } from "react";
+// import queryString from 'query-string';
+// import io from "socket.io-client";
 import TextContainer from '../TextContainer/TextContainer';
 import Messages from '../Messages/Messages';
 import InfoBar from '../InfoBar/InfoBar';
 import Input from '../Input/Input';
 
 import './Chat.css';
+import GameContext from "../../utils/GameContext";
 
-let socket;
-
-const Chat = ({ location }) => {
-  const [name, setName] = useState('');
-  const [room, setRoom] = useState('');
-  const [users, setUsers] = useState('');
-  const [message, setMessage] = useState('');
-  const [messages, setMessages] = useState([]);
-  // const ENDPOINT = 'https://project-chat-application.herokuapp.com/';
-  const ENDPOINT = 'http://localhost:5000';
-
-  
-
-  useEffect(() => {
-    const { name, room } = queryString.parse(location.search);
-
-    socket = io(ENDPOINT);
-
-    setRoom(room);
-    setName(name)
-
-    socket.emit('join', { name, room }, (error) => {
-      if(error) {
-        alert(error);
-      }
-    });
-  }, [ENDPOINT, location.search]);
-  
-  useEffect(() => {
-    socket.on('message', message => {
-      setMessages(messages => [ ...messages, message ]);
-    });
-    
-    socket.on("roomData", ({ users }) => {
-      setUsers(users);
-    });
-}, []);
-
-  const sendMessage = (event) => {
-    event.preventDefault();
-
-    if(message) {
-      socket.emit('sendMessage', message, () => setMessage(''));
-    }
-  }
+const Chat = () => {
+  const { users, name, room, message, messages, setMessage, sendMessage } = useContext(GameContext)
 
   return (
     <div className="outerContainer">
--- client/src/components/SettingsContainer/SettingsContainer.js
+++ client/src/components/SettingsContainer/SettingsContainer.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { useContext} from 'react';
 import AddWordContainer from '../AddWordContainer/AddWordContainer';
 import WordBankContainer from "../WordBankContainer/WordBankContainer"
-
+import GameContext from "../../utils/GameContext"
 
 const SettingsContainer = () => {
-
+    const { handleStartBtn } = useContext(GameContext)
     return(
        <div>
             <AddWordContainer/>
             <WordBankContainer/>
-            <button>Start Game</button>
+            <button onClick={handleStartBtn}>Start Game</button>
        </div>
     );
 }
