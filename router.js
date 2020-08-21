9afaf52125b0d6e31380bb6161136c616408237e
CofChips <qiwei.mod@gmail.com>
2020-08-21 14:08:17 -0700

    updated router.js to include code for production

--- router.js
+++ router.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
+if (process.env.NODE_ENV === "production") {
+  router.use(function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/build/index.html"))
+  })
+}
+
+
 router.get("/", (req, res) => {
   res.send({ response: "Server is up and running." }).status(200);
 });
+
 
 module.exports = router;
