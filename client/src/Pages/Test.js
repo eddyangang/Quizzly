1b024673b0e042d9fee0c2a8bdfc8f5d0221ba7e
CofChips <qiwei.mod@gmail.com>
2020-08-22 15:05:27 -0700

    reformatting for test

--- client/src/Pages/Test.js
+++ client/src/Pages/Test.js
@@ -7,26 +7,12 @@ import InForm from "../components/InForm/InForm";
 
 const Test = () => (
   <div>
-    <div>
       <div className="container-fluid mt-5">
         <div className="row">
           <div className="col-lg-3"></div>
           <div className="col-lg-6">
             <div className="jumbotron">
-
-
               <h1 className="text-center">Log In</h1>
-              {/* <form onSubmit={handleLogin}>
-<label>
-Email
-<input name="email" type="email" placeholder="Email" />
-</label>
-<label>
-Password
-<input name="password" type="password" placeholder="Password" />
-</label>
-<button type="submit">Log in</button>
-</form> */}
               <br />
               <center><a href="/signup">Create A New Account</a></center>
               <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
@@ -35,7 +21,6 @@ Password
           <div className="col-lg-3"></div>
         </div>
       </div>
-    </div>
   </div>
 );
 
