11494e8f0dd0abca314da3b59fa05257c998faf8
CofChips <qiwei.mod@gmail.com>
2020-08-22 14:19:02 -0700

    adding more components to test

--- client/src/Pages/Test.js
+++ client/src/Pages/Test.js
@@ -2,13 +2,42 @@ import React from 'react';
 import { withRouter, Redirect } from "react-router";
 import { firebase, uiConfig } from "../firebaseConfig"
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import InForm from "../components/InForm/InForm";
 
 
 const Test = () => (
   <div>
-      <h1>This is a test to see if something will render</h1>
+    <div>
+      <div className="container-fluid mt-5">
+        <div className="row">
+          <div className="col-lg-3"></div>
+          <div className="col-lg-6">
+            <div className="jumbotron">
 
-      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+
+              <h1 className="text-center">Log In</h1>
+              <InForm/>
+              {/* <form onSubmit={handleLogin}>
+<label>
+Email
+<input name="email" type="email" placeholder="Email" />
+</label>
+<label>
+Password
+<input name="password" type="password" placeholder="Password" />
+</label>
+<button type="submit">Log in</button>
+</form> */}
+              <br />
+              <center><a href="/signup">Create A New Account</a></center>
+              <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+            </div>
+          </div>
+          <div className="col-lg-3"></div>
+        </div>
+
+      </div>
+    </div>
   </div>
 );
 
