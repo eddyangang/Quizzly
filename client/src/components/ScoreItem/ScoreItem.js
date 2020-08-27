dc6c0ef1027aefa37c632c5db97df3e0a2d366e4
LoneStar <ernieurzua@gmail.com>
2020-08-27 13:42:01 -0700

    added score container

--- client/src/components/ScoreItem/ScoreItem.js
+++ client/src/components/ScoreItem/ScoreItem.js
@@ -1,12 +1,14 @@
 import React, { useContext } from 'react'
 import GameContext from "../../utils/GameContext"
+
+
 export default function ScoreItem() {
     const { users } = useContext(GameContext)
     return (
         <tbody className="gray">
-            {(users.map((score) => (
+            {(users.map((user) => (
                 <tr key={user.score}>
-                    <td className="mb-3 text-wrap">{score}</td>
+                    <td className="mb-3 text-wrap">{user.score}</td>
                 </tr>
             )))}
         </tbody>
